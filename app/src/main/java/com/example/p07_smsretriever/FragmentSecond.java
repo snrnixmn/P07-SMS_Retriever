f7cbc8836fbb1f0292ed202ce3c4c124bcbc1f91
18016339 <18016339@myrp.edu.sg>
2020-06-04 18:12:05 +0800

    done

---
+++ app/src/main/java/com/example/p07_smsretriever/FragmentSecond.java
@@ -0,0 +1,94 @@
+package com.example.p07_smsretriever;
+
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.text.format.DateFormat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link FragmentSecond#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FragmentSecond extends Fragment {
+
+    Button btnRetrieve2;
+    TextView tvFrag2;
+    EditText etWord;
+
+    public FragmentSecond() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
+
+        tvFrag2 = view.findViewById(R.id.tvFrag2);
+        btnRetrieve2 = view.findViewById(R.id.btnRetrieve2);
+        etWord = view.findViewById(R.id.etWord);
+
+        btnRetrieve2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String data = etWord.getText().toString();
+                // Create all messages URI
+                Uri uri = Uri.parse("content://sms");
+                // The columns we want
+                //  date is when the message took place
+                //  address is the number of the other party
+                //  body is the message content
+                //  type 1 is received, type 2 sent
+                String[] reqCols = new String[]{"date", "address", "body", "type"};
+
+                // Get Content Resolver object from which to
+                //  query the content provider
+                ContentResolver cr = getActivity().getContentResolver();
+
+                // The filter String
+                String filter = "body LIKE ?";
+                // The matches for the ?
+                String[] filterArgs = {"%" + data + "%" };
+                // Fetch SMS Message from Built-in Content Provider
+
+                Cursor cursor = cr.query(uri, reqCols, filter, filterArgs, null);
+
+                String smsBody = "";
+
+                if (cursor.moveToFirst()) {
+                    do {
+                        long dateInMillis = cursor.getLong(0);
+                        String date = (String) DateFormat.format("dd MMM yyyy h:mm:ss aa", dateInMillis);
+                        String address = cursor.getString(1);
+                        String body = cursor.getString(2);
+                        String type = cursor.getString(3);
+                        if (type.equalsIgnoreCase("1")) {
+                            type = "Inbox:";
+                        } else {
+                            type = "Sent:";
+                        }
+                        smsBody += type + " " + address + "\n at " + date
+                                + "\n\"" + body + "\"\n\n";
+                    } while (cursor.moveToNext());
+                }
+                tvFrag2.setText(smsBody);
+            }
+        });
+
+        return view;
+    }
+}
